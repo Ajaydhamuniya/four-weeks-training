aa96c374f1f29bc295707bb240d78b751b8dd863
Ajay Dhamuniya <aaa.dhamuniya@gmail.com>
2023-05-09 18:21:19 +0530

    ajay animal

--- week-1/day-2/exercise-3/AnimalExercise/Program.cs
+++ week-1/day-2/exercise-3/AnimalExercise/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace AnimalExercise
+﻿using System;
+using System.Collections.Generic;
+
+abstract class Animal
 {
-    internal class Program
+    public string Name;
+    public int Age;
+
+    public abstract void MakeSound();
+}
+
+class Dog : Animal, IMovable
+{
+    public override void MakeSound()
     {
-        static void Main(string[] args)
+        Console.WriteLine("ho ho");
+    }
+
+    public void Move()
+    {
+        Console.WriteLine("walking on four legs");
+    }
+}
+
+class Cat : Animal, IMovable
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+
+    public void Move()
+    {
+        Console.WriteLine("Jumping and climbing");
+    }
+}
+
+interface IMovable
+{
+    void Move();
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Animal> animals = new List<Animal>();
+        animals.Add(new Dog() { Name = "Buddy", Age = 4 });
+        animals.Add(new Cat() { Name = "Whiskers", Age = 2 });
+
+        foreach (Animal animal in animals)
         {
-            // Create a list of Animal objects, add Dog and Cat instances, and call their methods
+            Console.WriteLine($"{animal.Name} ({animal.GetType().Name}) is {animal.Age} years old:");
+            animal.MakeSound();
+
+            if (animal is IMovable)
+            {
+                Console.Write($"{animal.Name} is moving: ");
+                ((IMovable)animal).Move();
+            }
+
+            Console.WriteLine();
         }
+
+        Console.ReadLine();
     }
 }
