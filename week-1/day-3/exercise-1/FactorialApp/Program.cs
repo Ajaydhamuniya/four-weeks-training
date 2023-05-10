0ac3a9ff2333480de6e04ef9e7de7afa366a4374
Ajay Dhamuniya <aaa.dhamuniya@gmail.com>
2023-05-10 17:32:15 +0530

    factorial

--- week-1/day-3/exercise-1/FactorialApp/Program.cs
+++ week-1/day-3/exercise-1/FactorialApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FactorialApp
+﻿using System.Linq.Expressions;
+
+namespace FactorialApp
 {
     public class Program
     {
@@ -10,11 +12,32 @@
             long factorial = CalculateFactorial(number);
 
             Console.WriteLine($"The factorial of {number} is: {factorial}");
+            Console.WriteLine("press key");
+            Console.ReadKey();
         }
 
         public static long CalculateFactorial(int number)
-        {
-            throw new NotImplementedException();
+        { 
+
+            if(number < 0)
+            {
+                throw new ArgumentException();//expection of negative number given by user
+            }
+        
+
+            long factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+
+            //throw new NotImplementedException()          
+            
+
         }
     }
-}
+
+        
+    }
