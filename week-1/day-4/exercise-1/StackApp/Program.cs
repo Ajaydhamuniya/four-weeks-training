fb44f83aede4c09af8c39b23701315f2d38f7f8a
Ajay Dhamuniya <aaa.dhamuniya@gmail.com>
2023-05-11 15:59:47 +0530

    stack

--- week-1/day-4/exercise-1/StackApp/Program.cs
+++ week-1/day-4/exercise-1/StackApp/Program.cs
@@ -1,73 +1,113 @@
-﻿using System;
-using System.Collections.Generic;
-namespace StackApp
+﻿namespace StackApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private class Ad
         {
-            Stack<int> stack = new Stack<int>();
-            Stack<string> stack2 = new Stack<String>();
-            Console.WriteLine("Enter the  Integer Value of Stack");
-            stack.Push(Convert.ToInt32(Console.ReadLine()));
-            stack.Push(Convert.ToInt32(Console.ReadLine()));
-            stack.Push(Convert.ToInt32(Console.ReadLine()));
-            stack.Push(Convert.ToInt32(Console.ReadLine()));
-            Console.WriteLine("Enter the String Value:");
-            stack2.Push(Console.ReadLine());
-            stack2.Push(Console.ReadLine());
+            public int value;
 
-            Console.WriteLine("Elements in Stacks after Push in Integer:");
-            foreach (var item in stack)
+            public Ad(int value)
             {
-                Console.WriteLine(item.ToString());
-            }
-            Console.WriteLine("Elements in Stacks after Push in Integer:");
-            foreach (var item in stack2)
-            {
-                Console.WriteLine(item.ToString());
-            }
-
-            Console.WriteLine($"No of Element in Integer Stack:{stack.Count}");
-            Console.WriteLine($"No of Element in String Stack:{stack2.Count}");
-
-            stack.Pop();
-            stack2.Pop();
-            /* stack2.Pop()*/
-            ;
-            Console.WriteLine("ELment After Pop:");
-            
-
-            foreach (var item in stack)
-            {
-                Console.WriteLine(item);
-            }
-            foreach (var item in stack2)
-            {
-                Console.WriteLine(item);
-
-                if (stack.Count <= 0)
-                {
-                    Console.WriteLine("Stack is Empty");
-                }
-
-                if(stack2.Count<= 0)
-                {
-                    Console.WriteLine("string stack is empty");
-
-
-
-                }
-
-                //ICustomStack<int> intStack = new CustomStack<int>();
-                //intStack.Push(1);
-                //intStack.Push(2);
-                //intStack.Push(3);
-                //Console.WriteLine(intStack.Pop()); // Output: 3
-                //Console.WriteLine(intStack.Pop()); // Output: 2
-            
-                //Console.WriteLine(Stack.IsEmpty()) ; // Output: False
+                this.value = value;
             }
         }
-    } 
+
+        static void Main(string[] args)
+        {
+            //ICustomStack<int> intStack = new CustomStack<int>();
+            //intStack.Push(1);
+            //intStack.Push(2);
+            //intStack.Push(3);
+            //Console.WriteLine(intStack.Pop()); // Output: 3
+            //Console.WriteLine(intStack.Pop()); // Output: 2
+            //Console.WriteLine(intStack.IsEmpty()); // Output: False
+
+
+            CustomStack<int> stack = new CustomStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.IsEmpty());
+
+
+
+            CustomStack<string> stack2 = new CustomStack<string>();
+            stack2.Push("ajay");
+            stack2.Push("hrithik");
+            stack2.Push("rahul");
+            stack2.Push("sanu");
+
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.IsEmpty());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.IsEmpty());
+
+
+
+            CustomStack<Ad> stack3 = new CustomStack<Ad>();
+            stack3.Push(new Ad(1));
+            stack3.Push(new Ad(2));
+            stack3.Push(new Ad(3));
+            stack3.Push(new Ad(4));
+            stack3.Push(new Ad(5));
+            stack3.Push(new Ad(6));
+
+
+
+            Console.WriteLine(stack3.Pop());
+            Console.WriteLine(stack3.Pop());
+            Console.WriteLine(stack3.IsEmpty());
+            Console.WriteLine(stack3.Pop());
+            Console.WriteLine(stack3.Pop());
+            Console.WriteLine(stack3.IsEmpty());
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class CustomStack<T>
+    {
+        private List<T> list;
+        private int size;
+
+        public CustomStack()
+        {
+            list = new List<T>();
+            size = 0;
+        }
+
+        public void Push(T item)
+        {
+            list.Add(item);
+            size++;
+        }
+
+        public T Pop()
+        {
+            if (this.IsEmpty())
+            {
+                throw new IndexOutOfRangeException();
+            }
+            T value = list[size - 1];
+            list.RemoveAt(size - 1);
+            size--;
+            return value;
+        }
+
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+
+    }
 }
